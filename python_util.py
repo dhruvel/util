2af1494fddf060cd4a067050f83924f51339f372
Dhruv Sagar <53065041+dhruvel@users.noreply.github.com>
2025-06-13 21:41:18 +0530

    Deleted misc files

--- python_util.py
+++
@@ -1,7 +0,0 @@
-# Declare an array of string with libraries to be installed
-libraries = ["numpy", "scapy", "nmap", "requests", "pycrypto", "pandas", "matplotlib", "scipy", "sklearn", "seaborn", "jupyter", "jupyterlab", "tensorflow", "keras", "opencv-python", "pillow", "h5py", "requests", "beautifulsoup4", "pyyaml", "flask", "django", "pyqt5", "pyqtgraph", "pyopengl", "pyopengl-accelerate", "pyqtwebengine", "pyqtchart", "pyqtsensors", "pyqtdatavisualization", "pyqtmultimedia", "pyqtxmlpatterns", "pyqtxml", "pyqtnetwork", "pyqtsql", "pyqtuitools", "pyqtopengl", "pyqtopengl", "pyqtsvg", "pyqtx11ex"]
-
-# Iterate the string array using for loop
-for library in libraries:
-    print(f"Installing {library}")
-    !pip install {library}
